0a3dcf6231ae0a9eda8b73d703467dff29fe3a59
RAFID MD AZROF HOSSAIN <129204789+RAFID-MD-AZROF-HOSSAIN@users.noreply.github.com>
2023-06-19 23:24:49 +0800

    Update CategoriesController.cs

--- Tourismpk/Controllers/CategoriesController.cs
+++ Tourismpk/Controllers/CategoriesController.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Tourismpk.Models;
-
+//all good
 namespace Tourismpk.Controllers
 {
     public class CategoriesController : Controller
